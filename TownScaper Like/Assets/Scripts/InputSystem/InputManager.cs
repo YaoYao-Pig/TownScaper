31a5af9ab51824f290d98f6be840d0925ac6e3b2
摇摇猪 <73538420+YaoYao-Pig@users.noreply.github.com>
2024-08-05 00:44:04 +0800

    update:BuildingSystem

--- TownScaper Like/Assets/Scripts/InputSystem/InputManager.cs
+++ TownScaper Like/Assets/Scripts/InputSystem/InputManager.cs
@@ -58,56 +58,49 @@ public class InputManager : MonoBehaviour
             
             if (groundColliderQuad)
             {
-                Vector3 x = hit.point;
+                selectedVertex = null;
 
-                Vector3 a = groundColliderQuad.subQuad.a.currentWorldPosition;
-                Vector3 b = groundColliderQuad.subQuad.b.currentWorldPosition;
-                Vector3 c = groundColliderQuad.subQuad.c.currentWorldPosition;
-                Vector3 d = groundColliderQuad.subQuad.d.currentWorldPosition;
-                Vector3[] vextecs = new Vector3[] { a, b, c, d };
+                Vector3 aim = hit.point;
+                Quad sq = groundColliderQuad.subQuad;
+                Vector3 a = sq.a.currentWorldPosition;
+                Vector3 b = sq.b.currentWorldPosition;
+                Vector3 c = sq.c.currentWorldPosition;
+                Vector3 d = sq.d.currentWorldPosition;
+                Vector3 ab = (a + b) / 2;
+                Vector3 bc = (b + c) / 2;
+                Vector3 cd = (c + d) / 2;
+                Vector3 da = (d + a) / 2;
 
-                Vector3 ab = groundColliderQuad.subQuad.ab.midVertex.currentWorldPosition;
-                Vector3 bc = groundColliderQuad.subQuad.bc.midVertex.currentWorldPosition;
-                Vector3 cd = groundColliderQuad.subQuad.cd.midVertex.currentWorldPosition;
-                Vector3 da = groundColliderQuad.subQuad.da.midVertex.currentWorldPosition;
-                Vector3[] mids = new Vector3[] { ab, bc, cd, da };
 
-                Vector3 center = groundColliderQuad.subQuad.centerVertex.currentWorldPosition;
+                float ab_cd=(aim.z - ab.z)*(aim.x - cd.x) - (aim.z - cd.z)*(aim.x - ab.x);
+                float bc_da=(aim.z -bc.z)*(aim.x- da.x) - (aim.z - da.z)*(aim.x-bc.x);
 
-                int flag = 0;
-                for (int i = 0; i < 4; ++i)
+                float a_ab_cd = (a.z - ab.z)*(a.x-cd.x) - (a.z - cd.z) * (a.x-ab.x);
+                float a_bc_da = (a.z -bc.z)*(a.x-da.x)-(a.z - da.z)*(a.x-bc.x);
+                bool on_ad_side=ab_cd*a_ab_cd >= 0;
+                bool on_ab_side=bc_da*a_bc_da >= 0;
+                if (on_ab_side && on_ad_side)
                 {
-                    Vector3 a_ab = mids[i] - vextecs[i];
-                    Vector3 ab_center = center - mids[i];
-                    Vector3 center_ad = mids[(3 + i) % 4] - center;
-                    Vector3 ad_a = vextecs[i] - mids[(3 + i) % 4];
-
-                    Vector3 a_x = x - vextecs[i];
-                    Vector3 ab_x = x - mids[i];
-                    Vector3 center_x = x - center;
-                    Vector3 ad_x = x - mids[(3 + i) % 4];
-
-                    a_ab = Vector3.Cross(a_ab, a_x);
-                    ab_center = Vector3.Cross(ab_center, ab_x);
-                    center_ad = Vector3.Cross(center_ad, center_x);
-                    ad_a = Vector3.Cross(ad_a, ad_x);
-
-                    float a1 = Vector3.Dot(a_ab, ab_center);
-                    float a2 = Vector3.Dot(a_ab, center_ad);
-                    float a3 = Vector3.Dot(a_ab, ad_a);
-                    if (a1 <= 0 || a2 <= 0 || a3 <= 0) continue;
-                    flag = i; break;
+                    targetVertex = sq.a.yVertexList[1];
                 }
-
-                if (flag == 0) targetVertex = groundColliderQuad.subQuad.a.yVertexList[1];
-                else if (flag == 1) targetVertex = groundColliderQuad.subQuad.b.yVertexList[1];
-                else if (flag == 2) targetVertex = groundColliderQuad.subQuad.c.yVertexList[1];
-                else if (flag == 3) targetVertex = groundColliderQuad.subQuad.d.yVertexList[1];
+                else if (on_ad_side && !on_ab_side)
+                {
+                    targetVertex = sq.d.yVertexList[1];
+                }
+                else if (!on_ad_side && on_ab_side)
+                {
+                    targetVertex = sq.b.yVertexList[1];
+                }
+                else
+                {
+                    targetVertex = sq.c.yVertexList[1];
+                }
             }
             else
             {
                 SlotCollider slotCollider = hit.transform.GetComponentInParent<Transform>().GetComponentInParent<SlotCollider>();
                 //SlotSlideType type = hit.transform.GetComponent<SlotSlideType>();
+                selectedVertex = slotCollider.cubeVertex;
                 string type = hit.transform.GetComponent<Transform>().name;
                 CubeVertex cubeVertex = slotCollider.cubeVertex;
                 Debug.Log(type.ToString());
@@ -163,4 +156,100 @@ public class InputManager : MonoBehaviour
             Gizmos.DrawSphere(targetVertex.worldPosition, 0.2f);
         }
     }
+
+
+
+
+
+
+
+
+
+
+/*    private void FindTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxRayRange, layerMask))
+        {
+            GroundColliderQuad groundColliderQuad = hit.transform.GetComponent<GroundColliderQuad>();
+
+            if (groundColliderQuad)
+            {
+                Vector3 x = hit.point;
+
+                Vector3 a = groundColliderQuad.subQuad.a.currentWorldPosition;
+                Vector3 b = groundColliderQuad.subQuad.b.currentWorldPosition;
+                Vector3 c = groundColliderQuad.subQuad.c.currentWorldPosition;
+                Vector3 d = groundColliderQuad.subQuad.d.currentWorldPosition;
+                Vector3[] vextecs = new Vector3[] { a, b, c, d };
+
+                Vector3 ab = groundColliderQuad.subQuad.ab.midVertex.currentWorldPosition;
+                Vector3 bc = groundColliderQuad.subQuad.bc.midVertex.currentWorldPosition;
+                Vector3 cd = groundColliderQuad.subQuad.cd.midVertex.currentWorldPosition;
+                Vector3 da = groundColliderQuad.subQuad.da.midVertex.currentWorldPosition;
+                Vector3[] mids = new Vector3[] { ab, bc, cd, da };
+
+                Vector3 center = groundColliderQuad.subQuad.centerVertex.currentWorldPosition;
+
+                int flag = 0;
+                for (int i = 0; i < 4; ++i)
+                {
+                    Vector3 a_ab = mids[i] - vextecs[i];
+                    Vector3 ab_center = center - mids[i];
+                    Vector3 center_ad = mids[(3 + i) % 4] - center;
+                    Vector3 ad_a = vextecs[i] - mids[(3 + i) % 4];
+
+                    Vector3 a_x = x - vextecs[i];
+                    Vector3 ab_x = x - mids[i];
+                    Vector3 center_x = x - center;
+                    Vector3 ad_x = x - mids[(3 + i) % 4];
+
+                    a_ab = Vector3.Cross(a_ab, a_x);
+                    ab_center = Vector3.Cross(ab_center, ab_x);
+                    center_ad = Vector3.Cross(center_ad, center_x);
+                    ad_a = Vector3.Cross(ad_a, ad_x);
+
+                    float a1 = Vector3.Dot(a_ab, ab_center);
+                    float a2 = Vector3.Dot(a_ab, center_ad);
+                    float a3 = Vector3.Dot(a_ab, ad_a);
+                    if (a1 <= 0 || a2 <= 0 || a3 <= 0) continue;
+                    flag = i; break;
+                }
+
+                if (flag == 0) targetVertex = groundColliderQuad.subQuad.a.yVertexList[1];
+                else if (flag == 1) targetVertex = groundColliderQuad.subQuad.b.yVertexList[1];
+                else if (flag == 2) targetVertex = groundColliderQuad.subQuad.c.yVertexList[1];
+                else if (flag == 3) targetVertex = groundColliderQuad.subQuad.d.yVertexList[1];
+            }
+            else
+            {
+                SlotCollider slotCollider = hit.transform.GetComponentInParent<Transform>().GetComponentInParent<SlotCollider>();
+                //SlotSlideType type = hit.transform.GetComponent<SlotSlideType>();
+                string type = hit.transform.GetComponent<Transform>().name;
+                CubeVertex cubeVertex = slotCollider.cubeVertex;
+                Debug.Log(type.ToString());
+                if (type == "TOP")
+                {
+                    if (cubeVertex.y < Grid.maxY - 1)
+                    {
+                        targetVertex = selectedVertex.vertex.yVertexList[cubeVertex.y + 1];
+
+
+                    }
+                    else
+                    {
+                        targetVertex = null;
+                    }
+
+                }
+                else if (type == "SLIDE0" || type == "SLIDE1" || type == "SLIDE2" || type == "SLIDE3" || type == "SLIDE4")
+                {
+                    targetVertex = hit.transform.GetComponent<SlotSlide>().neighor;
+                }
+
+            }
+        }
+    }*/
+
 }
